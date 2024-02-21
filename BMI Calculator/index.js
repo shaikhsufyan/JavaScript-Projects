287187b1057f357baf161e39eccc00a271668ced
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-02-21 19:35:10 +0530

    BMI

--- BMI Calculator/index.js
+++ BMI Calculator/index.js
@@ -1,39 +1,34 @@
-let button = document.getElementById("calculate");
 let height = document.getElementById("height");
-// let inch = document.getElementById("inch");
 let weight = document.getElementById("weight");
-
-
-button.addEventListener("click", function(){
-    // formula :  kg/m*m
+let bmiShow = document.getElementById("bmi");
+let calculate = document.getElementById("calculate");
+let relode = document.getElementById("relode");
+ 
+calculate.addEventListener("click", function(){
+    // Formula BMI = kg/m*m
     let meter = height.value * 0.3048;
     let squareMeter = meter*meter;
+    let bmiCal = (weight.value/squareMeter).toFixed(2);
+    console.log(bmiCal);
+    bmiShow.innerHTML="Your BMI is : "+bmiCal;
 
-    let weightVal = weight.value;
-    let BMI = eval(weightVal/squareMeter)
-    console.log(BMI.toFixed(2));
-    document.getElementById("show").innerHTML="BMI : "+BMI.toFixed(2);
-
-    if(BMI<18){
-        document.getElementById("category").innerHTML="Under Weight";
-        document.getElementById("category").style.backgroundColor="rgb(255, 208, 1)";
+    if(bmiCal<18){
+        document.getElementById("category").innerHTML="You are Underweight";
+    }
+    else if(bmiCal>=18 && bmiCal<24){
+        document.getElementById("category").innerHTML="You are Normal Weight";
 
     }
-    else if(BMI>=18 && BMI<=24){
-        document.getElementById("category").innerHTML="Normal Weight";
-        document.getElementById("category").style.backgroundColor="rgb(3, 209, 3)";
+    else if(bmiCal>=24 && bmiCal<30){
+        document.getElementById("category").innerHTML="You are Above Weight";
 
-
-    }else if(BMI>=24 && BMI<=30){
-        document.getElementById("category").innerHTML="Over Weight";
-        document.getElementById("category").style.backgroundColor="rgb(255, 94, 0)";
-
-
-    }else if(BMI>30){
-        document.getElementById("category").innerHTML="Obese";
-        document.getElementById("category").style.backgroundColor="red";
-
+    }else if(bmiCal>=30){
+        document.getElementById("category").innerHTML="You are Obese";
 
     }
-  
+
+})
+
+relode.addEventListener("click", function(){
+    window.location.reload();
 })
