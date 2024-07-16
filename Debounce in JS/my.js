b332da6cb9e1647ef17d7e26662a613b87a01780
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-07-16 09:44:08 +0530

    debounce in JS

---
+++ Debounce in JS/my.js
@@ -0,0 +1,62 @@
+// let s = "   fly me   to   the moon  ";
+// let removeSpace = s.trim();
+ 
+// let array = removeSpace.split(" ");
+// console.log(array);
+// let last = array[array.length-1];
+// console.log(last);
+// console.log(last.length);
+
+
+// 2
+
+ 
+// convert 
+// const myFun = async () =>{
+    // let data = '{"p": 5}'
+    
+    // let jsonData = JSON.stringify(data);
+    // console.log();
+    // console.log(jsonData);
+// }
+// myFun()
+
+
+// let x = 10;
+
+// function myFun(){
+//     console.log("hello");
+// }
+// myFun()
+
+
+
+
+
+
+
+
+
+
+ let arr =[1,2,3,4,5];
+
+  let res= arr.some((cur,i, arr)=>{
+    return cur>11
+  })
+console.log(res);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
