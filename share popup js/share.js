bd3609ff55daa009c6e2b813634a8bac35b5973b
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-07-19 11:07:36 +0530

    share popup

--- share popup js/share.js
+++ share popup js/share.js
@@ -1,64 +1,19 @@
-let shareBtn = document.getElementById("share");
-let output = document.getElementById("output");
-let result = document.getElementById("result");
+ let button = document.getElementById("shareBtn");
+ let result = document.getElementById("result");
 
+ let data = {
+    title:"CODE With Sufyan Shaikh",
+    url:"https://www.youtube.com/results?search_query=code+with+sufyan",
+    text:"my channel"
+ }
 
-let data = {
-    title:"Share CODE With Sufyan Shaikh",
-    url:"https://www.lenskart.com/",
-    text:"Share Channel"
-}
-
-shareBtn.addEventListener("click",function(){
+ button.addEventListener("click", function(){
     try{
         navigator.share(data)
-        output.innerHTML="Link Shared!"
-
+        result.innerHTML="Link Shared"
     }catch(err){
-        console.log(err+"Succesfully Share");
+        console.log(err);
+        result.innerHTML="Error in Share"
 
     }
-
-})
-
-
-// Files
-// let input = document.getElementById("files");
-// let btn = document.getElementById("shareBtn");
-
- 
-// btn.addEventListener("click", async function(){
-
-//     let files  = input.files;
-//     if(files.length === 0){
-//         result.innerHTML="Please select file";
-//     }
-
-//     if(!navigator.canShare){
-//         result.innerHTML="Browser Not Support method";
-
-//     }
-
-//     let objData = {
-//         files,
-//         title:"File Sharing",
-//         text:"file"
-//     }
-  
-//     if(navigator.canShare({files})){
-         
-//         try{
-//             await navigator.share({
-//                 files,
-//                 title:"Share File"
-//             });
-//             result.innerHTML="File Shared."
-//         }catch(err){
-//             result.innerHTML=err
-//         }
-         
-//     }else{
-//         result.innerHTML="File not"
-
-//     }
-// })
+ })
