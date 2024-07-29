1b3f18a20ced1b89d090c0dc6b0805ea3482a3e5
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-07-29 09:00:29 +0530

    js

--- JS Shorts/index.js
+++ JS Shorts/index.js
@@ -1,3 +1,54 @@
+// What is shadowing
+
+ var a = 100;
+ 
+ var a = 200;
+ console.log(a);
+  
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Math.trun() Method
-let num = 4.99;
-console.log(Math.trunc(num));
+// let num = 4.99;
+// console.log(Math.trunc(num));
