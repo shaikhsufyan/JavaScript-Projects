b66588a607bf7ec65a0e1d5f09d01b22cb08545d
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-08-03 09:56:50 +0530

    JS interview question

---
+++ challenge1/index.js
@@ -0,0 +1,74 @@
+// add1()
+// const add1 = function (){
+//     console.log("add1 function ");
+// }
+
+// add2()
+// function add2(){
+//     console.log("add2 function");
+// }
+
+// let arr1 = [1,2,3];
+// let arr2 = [...arr1,4,5];
+// console.log(arr2);
+
+
+
+
+
+
+
+
+
+
+// let a = 5.5;
+// console.log(Math.floor(a));
+
+// let arr = ["CODE", "SUFYAN", "SHAIKH"];
+// console.log(arr[arr.length-1]);
+
+
+// console.log(x);
+// let x = 10;
+ 
+
+
+
+// function myFun(){
+//     let arr = [1,2,3,4,5];
+//     arr.length = 3
+//     console.log(arr);
+    
+// }
+// myFun()
+
+
+
+// let x = 30;
+// let x = 50;
+ 
+// console.log(x);
+
+
+// function(){
+//     statement
+// }
+// myFun()
+
+
+
+
+
+
+
+
+
+
+
+
+function myFun(){
+    x = 10;
+    console.log(x);
+}
+let x;
+myFun()
