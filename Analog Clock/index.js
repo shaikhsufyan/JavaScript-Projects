92c2c3c3d15b16a4e895aec2adc5c0232d5622bb
shaikh sufyan <shaikhsufyan1811@gmail.com>
2025-02-09 10:53:04 +0530

    Analog Clock Using JS

--- Analog Clock/index.js
+++ Analog Clock/index.js
@@ -1,17 +1,15 @@
 setInterval(()=>{
     let time = new Date();
-    let hour = time.getHours();
-    let minute = time.getMinutes();
-    let second = time.getSeconds();
-    
-    let hRotation = 30*hour+minute/2
-    let mRotation = 6*minute
-    let sRotation = 6*second
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
 
-    document.getElementById("hour").style.transform=`rotate(${hRotation}deg)`
-    document.getElementById("minute").style.transform=`rotate(${mRotation}deg)`
-    document.getElementById("second").style.transform=`rotate(${sRotation}deg)`
+    let hRotation = 30*hours+minutes/2;
+    let mRotation = 6*minutes;
+    let sRotation = 6*seconds;
 
-
+    document.getElementById("hour").style.transform=`rotate(${hRotation}deg)`;
+    document.getElementById("minute").style.transform=`rotate(${mRotation}deg)`;
+    document.getElementById("second").style.transform=`rotate(${sRotation}deg)`;
 
 },1000)
